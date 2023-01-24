ab680f18189d750399c7b0da2e91a41661b7db35
Yasin Mamedov <mamedov.iakh@phystech.edu>
2023-01-24 19:28:43 +0300

    v

--- main.cpp
+++ main.cpp
@@ -7,14 +7,11 @@ int main()
 
     char *buffer = ReadToBuffer("sheesh.txt", size);
 
-    Akinator_Info *akinator = GetAkinatorStruct(buffer, size);
+    char needed[] = "insane linking methods";
 
-    printf("lines total = %d:\n", akinator->lines_total);
-    for(int i = 0; i < akinator->lines_total; i++)
-    {
-        printf("%d: %s\n", i, akinator->Strings[i].text_ptr);
-    }
+    Tree *Akinator = CreateTree(needed);
 
+    FillTree(buffer, -1, Akinator->root);
 
     // printf("%s\n", NULL);
 
@@ -22,9 +19,5 @@ int main()
 
     // TreeDtor(akinator);
 
-    free(akinator->Strings);
-    free(akinator);
-    free(buffer);
-    
     return 0;
 }
